5f52c41ef7c0158c402d4b82a6e41f51ceafb1f5
Andy <hempel.andy@gmail.com>
2019-06-22 12:52:19 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# GT06/GPS103 MQTT Gateway
+This is a GT06 and GPS103 GPS Tracker server implementation  written in javascript.
+It parses all messages received from the device and creates the response message, if needed.
+Eventually it will send the received information to an MQTT broker.
+
+So it acts as a server for GT06 and GPS103 trackers and a gateway to MQTT.
+
+## Configuration
+Run a `npm install` after you cloned it and start it via `node app.js`.
+
+You can create a `.env` file to configure the behavior off the app.
+
+The following environment variables are recognized. If not defined a default will be used.
+- GT06_SERVER_PORT=64459
+- GPS103_SERVER_PORT=64460;
+- MQTT_ROOT_TOPIC=tracker
+- MQTT_BROKER_URL=localhost
+- MQTT_BROKER_PORT=8883
+- MQTT_BROKER_PROTO=mqtt
+- MQTT_BROKER_CA=/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
+- MQTT_BROKER_USER=user
+- MQTT_BROKER_PASSWD=passwd
+
+## MQTT
+Messages received on the TCP port will be transmitted via MQTT *MQTT_ROOT_TOPIC/IMEI/pos*
+
+For example: *tracker/123456789012345/pos*
---
+++ app.js
@@ -0,0 +1,112 @@
+require('dotenv').config();
+const Gt06 = require('gt06');
+const Gps103 = require('gps103');
+const Mqtt = require('mqtt');
+const net = require('net');
+const fs = require('fs');
+
+const gt06ServerPort = process.env.GT06_SERVER_PORT || 64459;
+const gps103ServerPort = process.env.GPS103_SERVER_PORT || 64460;
+const rootTopic = process.env.MQTT_ROOT_TOPIC || 'tracker';
+const brokerUrl = process.env.MQTT_BROKER_URL || 'localhost';
+const brokerPort = process.env.MQTT_BROKER_PORT || 8883;
+const mqttProtocol = process.env.MQTT_BROKER_PROTO || 'mqtt';
+const brokerUser = process.env.MQTT_BROKER_USER || 'user';
+const brokerPasswd = process.env.MQTT_BROKER_PASSWD || 'passwd';
+const trustedCaPath = process.env.MQTT_BROKER_CA || '';
+const TRUSTED_CA = fs.readFileSync(trustedCaPath);
+
+var mqttClient = Mqtt.connect(
+    {
+        host: brokerUrl,
+        port: brokerPort,
+        protocol: mqttProtocol,
+        ca: TRUSTED_CA,
+        username: brokerUser,
+        password: brokerPasswd
+    }
+);
+
+mqttClient.on('error', (err) => {
+    console.error('MQTT Error:', err);
+});
+
+var gt06Server = net.createServer((client) => {
+    var gt06 = new Gt06();
+    console.log('client connected');
+
+    gt06Server.on('error', (err) => {
+        console.error('gt06Server error', err);
+    });
+
+    client.on('error', (err) => {
+        console.error('client error', err);
+    });
+
+    client.on('close', () => {
+        console.log('client disconnected');
+    });
+
+    client.on('data', (data) => {
+        try {
+            gt06.parse(data);
+        }
+        catch (e) {
+            console.log('err', e);
+            return;
+        }
+        console.log(gt06);
+        if (gt06.expectsResponse) {
+            client.write(gt06.responseMsg);
+        }
+        gt06.msgBuffer.forEach(msg => {
+            mqttClient.publish(rootTopic + '/' + gt06.imei +
+                '/pos', JSON.stringify(msg));
+        });
+        gt06.clearMsgBuffer();
+    });
+});
+
+var gps103Server = net.createServer((client) => {
+    var gps103 = new Gps103();
+    console.log('client connected');
+
+    gps103Server.on('error', (err) => {
+        console.error('gps103Server error', err);
+    });
+
+    client.on('error', (err) => {
+        console.error('client error', err);
+    });
+
+    client.on('close', () => {
+        console.log('client disconnected');
+    });
+
+    client.on('data', (data) => {
+        try {
+            gps103.parse(data);
+        }
+        catch (e) {
+            console.log('err', e);
+            return;
+        }
+        console.log(gps103);
+        if (gps103.expectsResponse) {
+            client.write(gps103.responseMsg);
+        }
+        gps103.msgBuffer.forEach(msg => {
+            mqttClient.publish(rootTopic + '/' + gps103.imei +
+                '/pos', JSON.stringify(msg));
+        });
+        gps103.clearMsgBuffer();
+    });
+});
+
+gt06Server.listen(gt06ServerPort, () => {
+    console.log('started GT06 server on port:', gt06ServerPort);
+});
+
+gps103Server.listen(gps103ServerPort, () => {
+    console.log('started GPS103 gps103Server on port:', gps103ServerPort);
+});
